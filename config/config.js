08d3eaf993fdc4c161e61aafc68d088f3b0ab3d2
Sohidul-Islam <sishufol.sim@gmail.com>
2025-03-28 01:17:14 +0600

    add migration file

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config(); // Load environment variables from .env
 
 module.exports = {
   development: {
-    username: process.env.MYSQLUSERNAME_DEV || "root",
-    password: process.env.MYSQLPASSWORD_DEV || null,
-    database: process.env.DATABASE_DEV || "database_development",
-    host: process.env.MYSQLHOST_DEV || "127.0.0.1",
+    username: process.env.MYSQLUSERNAME_DEV,
+    password: process.env.MYSQLPASSWORD_DEV,
+    database: process.env.DATABASE_DEV,
+    host: process.env.MYSQLHOST_DEV,
     dialect: "mysql",
     logging: process.env.logging === "true", // Use boolean for logging
   },
@@ -17,10 +17,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.MYSQLUSERNAME || "root",
-    password: process.env.MYSQLPASSWORD || null,
-    database: process.env.DATABASE || "database_production",
-    host: process.env.MYSQLHOST || "127.0.0.1",
+    username: process.env.MYSQLUSERNAME,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.MYSQLHOST,
     dialect: "mysql",
   },
 };
